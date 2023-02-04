e85d6c7aa012f449193eaa3832c0a3f3733adbef
Voytesha <elflowers.info@gmail.com>
2023-02-04 22:20:24 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
+  
+let intervalId = null;
+refs.startBtn.addEventListener('click', backgroundColorChanging);
+refs.stopBtn.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function backgroundColorChanging() {
+    refs.startBtn.removeEventListener('click', backgroundColorChanging);  
+    refs.stopBtn.addEventListener('click', stopChanging);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopChanging() {
+    refs.stopBtn.removeEventListener('click', stopChanging);
+    refs.startBtn.addEventListener('click', backgroundColorChanging);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(intervalId);
+}
 
